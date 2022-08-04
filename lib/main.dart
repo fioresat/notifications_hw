d936b227c211a1dd2bb509662d916adb57ccc974
fioresat <mekhonoshinama@gmail.com>
2022-08-04 17:08:48 +0300

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -44,6 +44,10 @@ class _NotificationsPageState extends State<NotificationsPage> {
 
     _show
         ? WidgetsBinding.instance.addPostFrameCallback((_) async {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('Вы будуте получать ежедневное уведомление в 8-00!'),
+        duration: Duration(seconds: 2),
+        backgroundColor: Colors.indigo,),);
             await _scheduleDailyEightAMNotification();
           })
         : null;
@@ -73,18 +77,30 @@ class _NotificationsPageState extends State<NotificationsPage> {
           PaddedElevatedButton(
             buttonText: 'Получать повторяющиеся уведомления каждую минуту',
             onPressed: () async {
+              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                content: Text('Вы будуте получать уведомления раз в минуту!'),
+              duration: Duration(seconds: 2),
+              backgroundColor: Colors.indigo,),);
               await _repeatNotification();
             },
           ),
           PaddedElevatedButton(
             buttonText: 'Ежедневное уведомление в 10-00',
             onPressed: () async {
+              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                content: Text('Вы будуте получать ежедневное уведомление в 10-00!'),
+                duration: Duration(seconds: 2),
+                backgroundColor: Colors.indigo,),);
               await _scheduleDailyTenAMNotification();
             },
           ),
           PaddedElevatedButton(
             buttonText: 'Ежедневное уведомление в 8-00',
             onPressed: () async {
+              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                content: Text('Вы будуте получать ежедневное уведомление в 8-00!'),
+                duration: Duration(seconds: 2),
+                backgroundColor: Colors.indigo,),);
               _show = false;
               await _scheduleDailyEightAMNotification();
             },
